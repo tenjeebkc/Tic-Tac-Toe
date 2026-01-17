aa45c86b3c1b36e36552297379d7c72e0b187a8e
Tenjeeb K. C. <tenjeeb15@gmail.com>
2026-01-17 16:03:35 +0545

    Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const cells = document.querySelectorAll(".cell");
+const statusText =document.querySelector("#statusText");
+const restartBtn =document.querySelector("#restartBtn");
+const winCondition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+function initializeGame() {
+    cells.forEach(cell => cell.addEventListener("clicked", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`
+    
+}
+function cellClicked() {
+    
+}
+function updateCell(cell, index) {
+    
+}
+function changePlayer() {
+    
+}
+function checkWinner() {
+    
+}
+function restartGame() {
+    
+}
